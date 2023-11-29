97b6f1266ce73548d3d9bd080e0937616ef4b427
Clara <144264170+clara-tyman@users.noreply.github.com>
2023-11-29 12:57:25 -0500

    Initial files for MakeCode project

---
+++ main.py
@@ -0,0 +1,36 @@
+"""
+Created by: Clara
+Created on: Nov 2023
+This module is a Micro:bit MicroPython program
+"""
+
+from microbit import *
+
+
+display.scroll("Hello, World!")
+
+distance_to_object = number = 0
+
+# setup
+display.show(Image.HAPPY)
+
+while True:
+    if button_a.is_pressed() === true:
+        display.clear
+        while True:
+            # Distance from sonar
+
+            distance_to_object = sonar.ping(DigitalPin.P1, DigitalPin.P2, PingUnit.CENTIMETERS)
+
+    if distance_to_object < 10:
+        robotbit.stp_car_move(-10, 48)
+        display.pause(500)
+        robotbit.stepper_turn(robotbit.Steppers.M1, robotbit.Turns.T1B4)
+        display.pause(500)
+        robotbit.stepper_turn(robotbit.Steppers.M2, robotbit.Turns.T1B4)
+        display.pause(500)
+        robotbit.stp_car_move(10, 48)
+
+        else:
+        # move forward
+        robotbit.stp_car_move(10, 48)
